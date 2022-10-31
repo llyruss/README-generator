fb9188294daf6bf0dfd5d398de6c1ebb3770abf9
Lily Russ <llyruss@yahoo.com>
2022-10-30 22:44:48 -0500

    function to initialize

--- index.js
+++ index.js
@@ -54,10 +54,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.log(err) : console.log("Successfully created README.md"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(
+        (response) => {writeToFile("README.md", generateMarkdown(data))}
+        ) ;
+}
 
 // Function call to initialize app
 init();
